747dea93f7b72df2f96f3415a8fb1eacee029f5e
Nico Rithner <nicorithner@gmail.com>
2022-01-31 09:57:43 -0700

    Add examples for floating-Point and Numeric Operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -37,12 +37,34 @@ fn main() {
     // Scalar types: integers, floating-point numbers, Booleans, and characters.
     
     // Integers
-    let a: i32 = 90_222; // Decimal
-    let b: i32 = 0xff; // Hex
-    let c: i32 = 0o77; // Octal
-    let d: i32 = 0b1111_0000; // Binary
-    let e: u8 = b'A'; // Byte (u8 only)
+    // let a: i32 = 90_222; // Decimal
+    // let b: i32 = 0xff; // Hex
+    // let c: i32 = 0o77; // Octal
+    // let d: i32 = 0b1111_0000; // Binary
+    // let e: u8 = b'A'; // Byte (u8 only)
 
     // integer overflow
-    let f: u8 = 255; // maximum value for u8. 256 would cause integer overflow.
+    // let f: u8 = 255; // maximum value for u8. 256 would cause integer overflow.
+
+    // Floating-Point type
+    // let x = 2.0; // f64 is the default and has double precision
+    // let y: f32 = 3.0; // f32 is a single-precision float
+
+    //---- Numeric Operations
+    
+    // addition
+    let sum = 5 + 10;
+
+    // substraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // results in 0.
+    
+    // remainder
+    let remainder = 43 % 5;
 }
