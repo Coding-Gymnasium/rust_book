419640240437c4b3527b89318a34d5b090e333aa
Nico Rithner <nicorithner@gmail.com>
2022-02-05 18:08:45 -0700

    Complete section 3.2 of Chapter 3

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     //------ Muatable Variables
 
@@ -35,7 +37,7 @@ fn main() {
 
     //------ Data Types
     // Scalar types: integers, floating-point numbers, Booleans, and characters.
-    
+
     // Integers
     // let a: i32 = 90_222; // Decimal
     // let b: i32 = 0xff; // Hex
@@ -51,20 +53,101 @@ fn main() {
     // let y: f32 = 3.0; // f32 is a single-precision float
 
     //---- Numeric Operations
-    
+
     // addition
-    let sum = 5 + 10;
+    // let sum = 5 + 10;
+    //
+    // // substraction
+    // let difference = 95.5 - 4.3;
+    //
+    // // multiplication
+    // let product = 4 * 30;
+    //
+    // // division
+    // let quotient = 56.7 / 32.2;
+    // let floored = 2 / 3; // results in 0.
+    //
+    // // remainder
+    // let remainder = 43 % 5;
 
-    // substraction
-    let difference = 95.5 - 4.3;
+    ////---- Booleans
 
-    // multiplication
-    let product = 4 * 30;
+    // let t = true;
+    // let f: bool = false; // with explicit type annotation.
 
-    // division
-    let quotient = 56.7 / 32.2;
-    let floored = 2 / 3; // results in 0.
-    
-    // remainder
-    let remainder = 43 % 5;
+    ////---- Character
+
+    // let c = 'z';
+    // let z = 'ℤ';
+    // let heart_eyed_cat = '😻';
+
+    ////---- Compound Types
+    // Rust two primitive compound types:
+    // tuples and Arrays
+
+    //---- Tuple type
+    // A tuple is a general way of grouping together a number of values with a variety of type into
+    // one compound type. Tuples have a fixed length: once declare, they cannot grow or shrink in
+    // size.
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1); // the types are optional.
+
+    // Destructuring
+
+    let (_x, y, _z) = tup; // place underscores ahead of unused variables.
+    println!("the value of y is: {}", y);
+
+    // Dot notation (using a period)
+
+    let five_hundred = tup.0;
+    let _six_point_four = tup.1;
+    let _one = tup.2;
+
+    println!("First value is: {}", five_hundred);
+
+    //--- Array type
+    // arrays must have the same time. In Rust arrays must have fixed length.
+
+    let a = [1, 2, 3, 4, 5, 6];
+    // or
+    let a1: [i32; 6 ] = [7, 8, 9, 10, 11, 12]; // writing the array type.
+
+    // Accessing Array elements
+
+    let one = a[0];
+    let twelve = a1[5];
+    println!("one: {}, twelve: {}", one, twelve);
+
+    // Invalid array element access.
+    // Trying to access and index beyond the array's length causes Rust to panic. 
+    // example:
+
+    let b = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = b[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+
+    // output:
+    // Please enter an array index.
+    // 6
+    // thread 'main' panicked at 'index out of bounds: the len is 5 but the index is 6', src/main.rs:138:19
+    // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
 }
+
