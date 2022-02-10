0e458accf38e11dff2b44b3fea781ec00a79eb2b
Nico Rithner <nicorithner@gmail.com>
2022-02-10 09:08:39 -0700

    Complete Functions section

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,33 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
+
+
+// fn five() -> i32 {
+//     5
+// }
+//
+// fn main() {
+//     let x = five();
+//
+//     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     another_function(5, 'h');
+//     // let y = 6;
+//     let y = {
+//         let x = 3;
+//         x + 1
+//     };
+//     println!("The value of y is: {}", y);
+// }
+//
+// fn another_function(value: i32, unit_label: char) {
+//     println!("The value of x is: {}{}", value, unit_label);
+// }
