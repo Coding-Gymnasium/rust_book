f8101a3acdb739b0f281a4ed2595d28302fcb69e
Nico Rithner <nicorithner@gmail.com>
2022-03-17 16:30:42 -0600

    Complete sectiom 'Memory Allocation'

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-   // let s = "hello"; // type string literal
-    let mut s = String::from("hello"); // type String
-    s.push_str(", world!"); // push_str() appends a literal to a String
-    println!("{}", s);
+    // let s = "hello"; // type string literal // cannot be mutated
+    // let mut s = String::from("hello"); // type String
+    // s.push_str(", world!"); // push_str() appends a literal to a String
+    // println!("{}", s);
+
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+    //
+    // println!("{}, world", s1); // this erros out because s1 value was 'moved'to s2
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    // However is different with integers, because types like integers have a known size at compile.
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
 }
