63b057cb6d211c6dbee5e27ca26d353b67e06940
Nico Rithner <nicorithner@gmail.com>
2022-03-23 07:36:45 -0600

    Complete section Ownwership and Functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -18,4 +18,27 @@ fn main() {
     let y = x;
 
     println!("x = {}, y = {}", x, y);
-}
+
+    // Ownership and Functions
+    let s = String::from("hello"); // s comes into scope
+    // s's value moves into the function
+    // and so is no longer valid here
+    takes_ownership(s); 
+
+    let x = 5; // x comes into scope
+
+    // println!("{}", s); // this throws a compile-time error because swas moved to takes_ownership()
+
+    makes_copy(x)
+    // x would move int the function, but i32 is Copy, 
+    // so it's okay to still user x afterwards
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope.
+    println!("{}", some_string);
+} // Here, some_string goutes out of scope and 'drop' is called. The backing memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope.
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens
